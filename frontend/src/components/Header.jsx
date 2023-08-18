4fdbb3e31f5022f0f81bc5c66896da66cfaaad74
abigail-143 <abigailtoh1403@gmail.com>
2023-08-18 18:27:47 +0800

    create frontend Header component and stylesheet
    create both headers, one for welcome/register/login page and another for userpage/submitcontent/explorepage

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styles from "./Header.module.css"
+
+const Header = (props) => {
+  return (
+    <>
+      {props.showWelcome && (
+        <div className={styles.header}>
+          <img
+            src="https://picsum.photos/200"
+            width="50"
+            height="50"
+            className={styles.appLogo}
+          ></img>
+          <div className={styles.appName}>
+            <p>Better Time, Beer Time</p>
+          </div>
+          <button className={styles.registerBtn}>Register</button>
+          <button className={styles.loginBtn}>Login</button>
+        </div>
+      )}
+      {!props.showWelcome && (
+        <div className={styles.header}>
+          <img
+            src="https://picsum.photos/200"
+            width="50"
+            height="50"
+            className={styles.appLogo}
+          ></img>
+          <div className={styles.appName}>
+            <p>Better Time, Beer Time</p>
+          </div>
+          <input className={styles.searchBar} placeholder="hello"></input>
+          <img
+            src="https://picsum.photos/200"
+            width="40"
+            height="40"
+            className={styles.likesIcon}
+          ></img>
+          <img
+            src="https://picsum.photos/200"
+            width="40"
+            height="40"
+            className={styles.profilePic}
+          ></img>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Header;
