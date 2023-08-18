0230f4791822ed77aa7485c452ef13fbdb1b8014
chunyean <chunyeanfoong@gmail.com>
2023-08-18 13:34:16 +0800

    update controller's content

--- backend/src/controllers/content.js
+++ backend/src/controllers/content.js
@@ -1,26 +1,28 @@
 // create controller for all content
+const Content = require("../models/Content");
 const ContentModel = require("../models/Content");
 const UserModel = require("../models/User");
 
 // create new content
 const createNewContent = async (req, res) => {
   try {
-    const userId = UserModel.findById(req.params.user_id);
+    const userId = await UserModel.findById(req.params.id);
 
     const content = new ContentModel({
       contentPhoto: req.body.contentPhoto,
-      contentPhoto: req.body.contentPhoto,
+      drinkName: req.body.drinkName,
       shopName: req.body.shopName,
       contentReview: req.body.contentReview,
       contentTag: req.body.contentTag,
       comments: req.body.comments,
       likeCount: req.body.likeCount,
+      userID: userId._id,
     });
 
-    userId.ContentModel.push(content);
-
+    userId.createdContent.push(content);
+    await content.save();
     await userId.save();
-
+    // res.json(content);
     res.json(userId);
   } catch (error) {
     console.log(error.message);
@@ -31,7 +33,9 @@ const createNewContent = async (req, res) => {
 //delete user's content
 const deleteContent = async (req, res) => {
   try {
-    await ContentModel.findByIdAndDelete(req.params.user._id);
+    const user = await UserModel.findById(req.params.id)
+    await user.ContentModel.findByIdAndDelete(req.body.id);
+
     res.json({ status: "ok", msg: "Content deleted" });
   } catch (error) {
     console.log(error.message);
@@ -40,14 +44,29 @@ const deleteContent = async (req, res) => {
 };
 
 //get all the user's content
-// const getContent = async (req, res) => {
-//   try {
-//     const content = await UserModel.find();
-//     res.json(content);
-//   } catch (error) {
-//     console.log(error.message);
-//     res.json({ status: "error", msg: error.message });
-//   }
-// };
+const getContent = async (req, res) => {
+  try {
+    const content = await UserModel.findById(req.params.id);
+    res.json(content.createdContent);
+  } catch (error) {
+    console.log(error.message);
+    res.json({ status: "error", msg: error.message });
+  }
+};
 
-module.exports = { getContent, createNewContent, deleteContent };
+//update user's content
+const updateContent = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error.message);
+    res.json({ status: "error", msg: error.message });
+  }
+};
+
+
+module.exports = {
+  getContent,
+  createNewContent,
+  deleteContent,
+  updateContent,
+};
