f96d886b7f9207956b14875e97636da5882463f4
abigail-143 <abigailtoh1403@gmail.com>
2023-08-18 18:26:45 +0800

    set up frontend App.jsx
    include useStates boolean to determine which components should be showing.
    
    set up skeleton for final webpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-
-
+  // const [showRegister, setShowRegister] = useState(false);
+  // const [showLogin, setShowLogin] = useState(false);
+  const [showWelcome, setShowWelcome] = useState(false);
+  // const [showLandingPage, setShowLandingPage] = useState(true);
+  // const [showExplorePage, setShowExplorePage] = useState(false);
+  // const [showUserPage, setShowUserPage] = useState(false);
+  // const [showSubmitContent, setShowSubmisContent] = useState(false);
   return (
     <>
-      
+      <Header showWelcome={showWelcome}></Header>
+      {/* {showWelcome && <WelcomePage></WelcomePage>}
+      {showLogin && <Login></Login>}
+      {showRegister && <Register></Register>}
+      {showLandingPage && <LandingPage></LandingPage>}
+      {showExplorePage && <ExplorePage></ExplorePage>}
+      {showUserPage && <UserPage></UserPage>}
+      {showSubmitContent && <SubmitContent></SubmitContent>} */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+// <Header />
+// <WelcomePage />
+// <LandingPage />
+// <ExplorePage />
+// <UserPage />
+// <ExploreContentDisplay />
+// <UserContentDisplay />
+// <SubmitContent />
+// <Register />
+// <Login />
