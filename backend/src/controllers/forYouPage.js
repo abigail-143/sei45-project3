ff009e434d1f58ed9bd4ed0ecd6994f88be35600
abigail-143 <abigailtoh1403@gmail.com>
2023-08-18 10:12:26 +0800

    create forYouPage controller
    added GET which retrieves all content from Content Collection
    added POT which retrieves one content from Content collection to display, using content Object _Id

---
+++ backend/src/controllers/forYouPage.js
@@ -0,0 +1,25 @@
+const ContentModel = require("../models/Content")
+const CommentModel = require("../models/Comment")
+const UserModel = require("../models/User")
+
+// GET - all content from all users
+const getAllContents = async (req, res) => {
+    try {
+        const allContents = await ContentModel.find()
+        res.json(allContents)
+    } catch (error) {
+        console.log(error.message)
+        res.json({status: "error", msg: "failed to retrieve content data."})
+    }
+}
+
+// POST - get 1 content by contentId parma
+const getOneContentByContentID = async (req, res) => {
+    try {
+        const uniqueContent = await ContentModel.findById(req.params.contentId);
+        res.json(uniqueContent)
+    } catch (error) {
+        console.log(error.message)
+        res.json({status: "error", msg: "failed to retrieve content."})
+    }
+}
