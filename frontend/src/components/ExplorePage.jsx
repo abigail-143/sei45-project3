117726e95aebef9a4f45fdd4db13ab6413528492
abigail-143 <abigailtoh1403@gmail.com>
2023-08-19 14:09:31 +0800

    update explorePage for content overlay and add in quickfilter bar

--- frontend/src/components/ExplorePage.jsx
+++ frontend/src/components/ExplorePage.jsx
@@ -3,24 +3,38 @@ import Masonry from "react-responsive-masonry";
 import testImgs from "./testImgArray";
 
 const ExplorePage = () => {
-  const hashtags = "#beer #scotchale #pilsner";
-  // fetch collection, and return exploreBlock
+  const tags = "#beer #scotchale #pilsner #draft #carlsberg";
 
+  const hashtags = tags.split(" ");
+  const hashtagItems = hashtags.map((hashtag, index) => {
+    return (
+      <li key={index} className="quickFilterItem">
+        <a href="/">{hashtag}</a>
+      </li>
+    );
+  });
+
+  // fetch collection, and return contentBlock
   const contentBlocks = testImgs.map((content, index) => {
     return (
-      <figure>
-        <div className="imgDiv">
-          <img key={index} src={content.contentPhoto}></img>
+      <figure key={index}>
+        <div className="imgDisplay">
+          <img src={content.contentPhoto}></img>
         </div>
-        <div className="detailDiv">
-          <p>{content.contentReview}</p>
+        <div className="detailDisplay">
+          <a href="/" target="_blank" className="usernameLink">
+            {content.username}
+          </a>
+          <img className="likeBtn" src="../public/heart.png"></img>
         </div>
       </figure>
     ); // need to hid the detailDiv, only show on hover and also make the position relative to the imgDiv
   });
   return (
     <>
-      <div className="quickFilter"></div>
+      <div className="quickFilter">
+        <ul className="quickFilterBar">{hashtagItems}</ul>
+      </div>
       <section className="explore">
         <Masonry columnsCount={4} gutter="10px">
           {contentBlocks}
--- frontend/src/components/testImgArray.js
+++ frontend/src/components/testImgArray.js
@@ -4,90 +4,105 @@ const testImgs = [
     contentPhoto:
       "https://images.unsplash.com/photo-1600111765736-9c59f7afe9e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVlciUyMGNhbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
     contentReview: "this is a review",
+    username: "@username",
   },
   {
     content: 2,
     contentPhoto:
       "https://images.unsplash.com/photo-1613904985222-0d534430bdbd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmVlciUyMGNhbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
     contentReview: "this is a review",
+    username: "@username",
   },
   {
     content: 3,
     contentPhoto:
       "https://images.unsplash.com/photo-1620316462488-117e453b398a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YmVlciUyMGNhbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
     contentReview: "this is a review",
+    username: "@username",
   },
   {
     content: 4,
     contentPhoto:
       "https://images.unsplash.com/photo-1586278523983-df3f2f68b7e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YmVlciUyMGNhbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
     contentReview: "this is a review",
+    username: "@username",
   },
   {
     content: 5,
     contentPhoto:
       "https://images.unsplash.com/photo-1522997169209-1629f3f89fdf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJlZXIlMjBjYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     contentReview: "this is a review",
+    username: "@username",
   },
   {
     content: 6,
     contentPhoto:
       "https://images.unsplash.com/photo-1585975772037-425dffcb3fd1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGJlZXIlMjBjYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     contentReview: "this is a review",
+    username: "@username",
   },
   {
     content: 7,
     contentPhoto:
       "https://images.unsplash.com/photo-1581036531354-a24b6822baa7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGJlZXIlMjBjYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     contentReview: "this is a review",
+    username: "@username",
   },
   {
     content: 8,
     contentPhoto:
       "https://images.unsplash.com/photo-1588704487282-e7c55e0448bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fGJlZXIlMjBjYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     contentReview: "this is a review",
+    username: "@username",
   },
   {
     content: 9,
     contentPhoto:
       "https://images.unsplash.com/photo-1559019736-dcf2caefe954?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fGJlZXIlMjBjYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     contentReview: "this is a review",
+    username: "@username",
   },
   {
     content: 10,
     contentPhoto:
       "https://images.unsplash.com/photo-1614537088976-ac906fb03011?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGJlZXIlMjBjYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     contentReview: "this is a review",
+    username: "@username",
   },
   {
     content: 11,
     contentPhoto:
       "https://images.unsplash.com/photo-1560952143-8a175051909b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDV8fGJlZXIlMjBjYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     contentReview: "this is a review",
+    username: "@username",
   },
   {
     content: 12,
     contentPhoto:
       "https://images.unsplash.com/photo-1578105042098-0637a8b9e5d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTB8fGJlZXIlMjBjYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     contentReview: "this is a review",
+    username: "@username",
   },
   {
     content: 13,
     contentPhoto:
       "https://images.unsplash.com/photo-1595863752443-f9e973f8066d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTN8fGJlZXIlMjBjYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     contentReview: "this is a review",
+    username: "@username",
   },
   {
     content: 14,
     contentPhoto:
       "https://images.unsplash.com/photo-1596090972032-1cfae1224535?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTZ8fGJlZXIlMjBjYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     contentReview: "this is a review",
+    username: "@username",
   },
   {
     content: 15,
     contentPhoto:
       "https://images.unsplash.com/photo-1596573324577-7731bdd32894?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTl8fGJlZXIlMjBjYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     contentReview: "this is a review",
+    username: "@username",
   },
 ];
 
