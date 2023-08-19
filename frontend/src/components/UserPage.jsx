f41e86370fd8eab6a73ce111b5b2a3730686b298
abigail-143 <abigailtoh1403@gmail.com>
2023-08-19 18:55:19 +0800

    update UserPage with it's own module css

--- frontend/src/components/UserPage.jsx
+++ frontend/src/components/UserPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import testImgs from "./testImgArray";
+import styles from "./UserPage.module.css";
 
 const UserPage = () => {
   // this state will determine if display is showing created content or liked content. use the tabs buttons to toggle this state
@@ -8,16 +9,16 @@ const UserPage = () => {
   // this is to pull the user's created content
   const createdContentBlocks = testImgs.map((content, index) => {
     return (
-      <div key={index} className="contentDisplay">
-        <div className="contentImg">
-          <img className="imgDisplay" src={content.contentPhoto}></img>
+      <div key={index} className={styles.contentDisplay}>
+        <div className={styles.contentImg}>
+          <img className={styles.imgDisplay} src={content.contentPhoto}></img>
         </div>
-        <div className="contentDetail">
-          <img className="icon" src="../public/heart.png"></img>
-          <label className="numLabel">{content.likeCount}</label>
-          <img className="icon" src="../public/comment.png"></img>
-          <label className="numLabel">{content.comments.length}</label>
-          <button className="deleteBtn">Delete</button>
+        <div className={styles.contentDetail}>
+          <img className={styles.icon} src="../public/heart.png"></img>
+          <label className={styles.numLabel}>{content.likeCount}</label>
+          <img className={styles.icon} src="../public/comment.png"></img>
+          <label className={styles.numLabel}>{content.comments.length}</label>
+          <button className={styles.deleteBtn}>Delete</button>
         </div>
       </div>
     );
@@ -26,19 +27,21 @@ const UserPage = () => {
   // this is to pull the user's liked content
   const likedContentBlocks = testImgs.map((content, index) => {
     return (
-      <div key={index} className="contentDisplay">
-        <div className="contentImg">
-          <img className="imgDisplay" src={content.contentPhoto}></img>
+      <div key={index} className={styles.contentDisplay}>
+        <div className={styles.contentImg}>
+          <img className={styles.imgDisplay} src={content.contentPhoto}></img>
         </div>
-        <div className="contentImgOverlay">
+        <div className={styles.contentImgOverlay}>
           <p>{content.username}</p>
         </div>
-        <div className="contentDetail">
-          <img className="favIcon" src="../public/heart.png"></img>
-          <label className="favNumLabel">{content.likeCount}</label>
-          <div className="divider"></div>
-          <img className="favIcon" src="../public/comment.png"></img>
-          <label className="favNumLabel">{content.comments.length}</label>
+        <div className={styles.contentDetail}>
+          <img className={styles.favIcon} src="../public/heart.png"></img>
+          <label className={styles.favNumLabel}>{content.likeCount}</label>
+          <div className={styles.divider}></div>
+          <img className={styles.favIcon} src="../public/comment.png"></img>
+          <label className={styles.favNumLabel}>
+            {content.comments.length}
+          </label>
         </div>
       </div>
     );
@@ -47,19 +50,21 @@ const UserPage = () => {
   return (
     <>
       {/* this div just pulls user's profilepic and username */}
-      <div className="userInfo">
+      <div className={styles.userInfo}>
         <img src="https://images.unsplash.com/photo-1519052537078-e6302a4968d4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNhdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"></img>
         <p>@username</p>
       </div>
       {/* this div are for the patch, edit user's info, and put, add content. */}
-      <div className="userBtn">
-        <button className="btn">Edit Profile</button>
-        <button className="btn">Add Post</button>
+      <div className={styles.userBtn}>
+        <button className={styles.btn}>Edit Profile</button>
+        <button className={styles.btn}>Add Post</button>
       </div>
       {/* this div is to navigate between created content and liked content */}
-      <div className="tabs">
+      <div className={styles.tabs}>
         <button
-          className={showCreated ? "tabBtn highlight" : "tabBtn"}
+          className={
+            showCreated ? `${styles.tabBtn} ${styles.highlight}` : styles.tabBtn
+          }
           onClick={() => {
             setShowCreated(true);
           }}
@@ -67,7 +72,9 @@ const UserPage = () => {
           Created
         </button>
         <button
-          className={showCreated ? "tabBtn" : "tabBtn highlight"}
+          className={
+            showCreated ? styles.tabBtn : `${styles.tabBtn} ${styles.highlight}`
+          }
           onClick={() => {
             setShowCreated(false);
           }}
@@ -76,8 +83,8 @@ const UserPage = () => {
         </button>
       </div>
       {/* this div is to display created content */}
-      <div className="displayContainer">
-        <div className="display">
+      <div className={styles.displayContainer}>
+        <div className={styles.display}>
           {showCreated ? createdContentBlocks : likedContentBlocks}
         </div>
       </div>
