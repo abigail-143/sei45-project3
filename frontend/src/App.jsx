31fc2643e69f8f36c1669cbb4db386b244dfb53f
abigail-143 <abigailtoh1403@gmail.com>
2023-08-21 13:28:08 +0800

    update app with components for login and landing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,17 @@ import "./App.css";
 import Header from "./components/Header";
 import ExplorePage from "./components/ExplorePage";
 import UserPage from "./components/UserPage";
+import Landing from "./components/Landing";
+import Login from "./components/authenticationOverlay/LoginModal"
 
 function App() {
   // const [showRegister, setShowRegister] = useState(false);
   const [showLogin, setShowLogin] = useState(false);
   const [showRegister, setShowRegister] = useState(false);
   const [accessToken, setAccessToken] = useState("");
-  const [showWelcome, setShowWelcome] = useState(false);
+  const [showWelcome, setShowWelcome] = useState(true);
   const [showExplorePage, setShowExplorePage] = useState(false);
-  const [showUserPage, setShowUserPage] = useState(true);
+  const [showUserPage, setShowUserPage] = useState(false);
   // const [showSubmitContent, setShowSubmisContent] = useState(false);
   return (
     <>
