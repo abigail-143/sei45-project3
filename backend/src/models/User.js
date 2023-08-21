e9234b7890df34478c1c482d883c189435ba4145
abigail-143 <abigailtoh1403@gmail.com>
2023-08-21 11:04:12 +0800

    update schema

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
-const ContentSchema = require('./Content')
+const ContentSchema = require("./Content");
 
 const UsersSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, minLength: 5, maxLength: 20 },
     hashPWD: { type: String, required: true },
     profilePhoto: { type: String },
+    likedContent: { type: Array }, // array of content _ids that user likes
   },
   { collection: "User" }
 );
