44dd47affd80aca59d078f9efe1dad3a5a00fd08
abigail-143 <abigailtoh1403@gmail.com>
2023-08-21 14:16:53 +0800

    update seeds

--- backend/src/routers-abi/forYouPage.js
+++ backend/src/routers-abi/forYouPage.js
@@ -10,7 +10,7 @@ const { auth } = require("../middleware/user");
 const router = express.Router();
 
 // router.get("/seed-users", seedUsers);
-router.get("/seed-contents", auth, seedContents);
+router.get("/seed-contents", seedContents);
 router.get("/all-contents", auth, getAllContents);
 router.post("/:contentId", auth, getOneContentByContentID);
 router.patch("/:contentId", auth, addToLikeCount);
