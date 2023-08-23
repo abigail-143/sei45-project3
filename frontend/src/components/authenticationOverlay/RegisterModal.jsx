f138b48561039c3f2937505b79e04c6e5381e420
chunyean <chunyeanfoong@gmail.com>
2023-08-23 17:11:15 +0800

    create upload photo function for profile photo

--- frontend/src/components/authenticationOverlay/RegisterModal.jsx
+++ frontend/src/components/authenticationOverlay/RegisterModal.jsx
@@ -11,6 +11,9 @@ const RegisterOverlay = (props) => {
   const [password, setPassword] = useState("");
   const [error1, setError1] = useState("");
   const [error2, setError2] = useState("");
+  const [uploadPic, setUploadPic] = useState(true);
+  const [photo, setPhoto] = useState("");
+  const [displayPic, setDisplayPic] = useState(false);
   const usernameRef = useRef("");
   const pwdRef = useRef("");
   const fetchData = useFetch();
@@ -35,9 +38,34 @@ const RegisterOverlay = (props) => {
       setError2(res.data[2]);
     }
   };
+
+  //function to convert photo file into base64
+  const convertToBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+
+  // convert file into base64 and standby for upload
+  const handleFileUpload = async (e) => {
+    setUploadPic(false);
+    const file = e.target.files[0];
+    const base64 = await convertToBase64(file);
+    setDisplayPic(true);
+    setPhoto(base64);
+  };
+
   return (
     <div className={styles.backdrop}>
       <div className={styles.modal}>
+        <div className={styles.welcomeText}>Welcome to BTBT</div>
         <button
           className={styles.closeButton}
           onClick={() => {
@@ -46,9 +74,24 @@ const RegisterOverlay = (props) => {
         >
           <img src="../../picture/Dell.jpg" />
         </button>
-        <div className={styles.profPic}>BTBT</div>
-        <div className={styles.welcomeText}>Welcome to BTBT</div>
-        <div className={styles.loginDetails}>
+        <label className={styles.form} htmlFor="profile-pic">
+          {uploadPic && (
+            <img
+              className={styles.profPhoto}
+              src="https://images.unsplash.com/photo-1552944150-6dd1180e5999?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1025&q=80"
+            />
+          )}
+          <input
+            type="file"
+            label="photo"
+            id="profile-pic"
+            className={styles.inputProfilePic}
+            accept=".jpeg, .png, .jpg"
+            onChange={(e) => handleFileUpload(e)}
+          ></input>
+          {displayPic && <img className={styles.displayPic} src={photo} />}
+        </label>
+        <div className={styles.loginDetails2}>
           <div>
             <div>Username:</div>
             <input
