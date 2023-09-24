38a9f1cdd5969588ec9d1c7ae544e50692141a84
abigail-143 <abigailtoh1403@gmail.com>
2023-09-24 14:59:26 +0800

    update forgotPassword modal

--- frontend/src/components/authenticationOverlay/ForgotPassword.jsx
+++ frontend/src/components/authenticationOverlay/ForgotPassword.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useContext, useState, useRef } from "react";
 import styles from "./Modal.module.css";
 import { useNavigate } from "react-router-dom";
+import AuthContext from "../context/auth";
+import useFetch from "../custom_hooks/useFetch";
 
 const ForgotPassword = (props) => {
   const navigate = useNavigate();
+  const auth = useContext(AuthContext);
+  const fetchData = useFetch();
+
+  const [username, setUsername] = useState(props.username);
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const pwRef = useRef("");
+  const confirmPWRef = useRef("");
+
+  const HandleResetPassword = async () => {
+    const res = await fetchData("/landing/reset", "POST", {
+      username,
+      password,
+    });
+
+    if (res.ok) {
+      console.log(res);
+      auth.setAccessToken(res.data.access);
+      alert("Password successfully updated");
+      navigate("/login");
+    } else {
+      console.log(res.data);
+    }
+  };
+
+  const CheckPW = () => {
+    if (confirmPWRef.current.value !== pwRef.current.value) {
+      setMessage("Passwords don't match");
+    } else {
+      setMessage("Passwords match");
+    }
+  };
+
   return (
     <div className={styles.backdrop}>
       <div className={styles.modalFPW}>
@@ -23,21 +59,33 @@ const ForgotPassword = (props) => {
         <div className={styles.divFPW}>
           <label className={styles.labelFPW}>New Password:</label>
           <input
+            ref={pwRef}
             className={styles.inputFPW}
             onChange={(e) => {
-              props.setPassword(e.target.value);
+              setPassword(e.target.value);
             }}
           ></input>
         </div>
         <div className={styles.divFPW}>
           <label className={styles.labelFPW}>Confirm Password:</label>
-          <input className={styles.inputFPW}></input>
+          <input
+            ref={confirmPWRef}
+            className={styles.inputFPW}
+            onChange={() => CheckPW()}
+          ></input>
+          <small
+            className={`${styles.labelFPW} ${
+              message.length == 15 ? styles.green : styles.red
+            }`}
+          >
+            {message}
+          </small>
         </div>
         <div className={styles.divFPW}>
           <button
             className={styles.buttonFPW}
             onClick={() => {
-              navigate("/login");
+              HandleResetPassword();
               props.setResetPW(false);
               console.log("hi");
             }}
